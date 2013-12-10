2a7267bb5d4b7b07cb0af1aa8be7a83bc20dcd30
AngeloFajardo <acf11f@my.fsu.edu>
2013-12-09 19:50:04 -0500

    added Dijkstra's algorithm

--- assignment3.py
+++ assignment3.py
@@ -2,6 +2,42 @@
 # COP4531
 # Assignment 3
 
+from collections import namedtuple
+
+inf = float('inf')
+Edge = namedtuple('Edge', 'start, end, cost')
+ 
+class Graph():
+    def __init__(self, edges):
+        self.edges = edges2 = [Edge(*edge) for edge in edges]
+        self.vertices = set(sum(([e.start, e.end] for e in edges2), []))
+ 
+    def dijkstra(self, source):
+        dist = {vertex: inf for vertex in self.vertices}
+        previous = {vertex: None for vertex in self.vertices}
+        dist[source] = 0
+        q = self.vertices.copy()
+        neighbours = {vertex: set() for vertex in self.vertices}
+        for start, end, cost in self.edges:
+            neighbours[start].add((end, int(cost)))
+            
+        #pp(neighbours)
+ 
+        while q:
+            u = min(q, key=lambda vertex: dist[vertex])
+            print(dist)
+            q.remove(u)
+            if dist[u] == inf:
+                break
+            for v, cost in neighbours[u]:
+                alt = dist[u] + cost
+                if alt < dist[v]:                                  # Relax (u,v,a)
+                    dist[v] = alt
+                    previous[v] = u
+
+        for x in dist:
+            print(x, dist[x])                   
+
 def main() :
     
     inputFile = open("input.txt")
@@ -12,14 +48,20 @@ def main() :
     if(first_line[0] == 'U'):           
         print ("Undirected\n")          
 
-    graph = {}
-    node = {}
+    graph = []
+    nodeList = {}
     
-    for line in inputFile:           
+    for line in inputFile:
         if line[0] != '#':
-            node = line
-            graph[node[0]] = node[2], node[4]
-            print (graph)
+            nodeList = tuple(line.split())
+            graph.append(nodeList)
+
+    graphs = Graph(graph)
+##    print(graphs.edges)
+##    print(graphs.vertices)
+
+    print(graphs.dijkstra('A'))
+
 
 if __name__ == '__main__':
 
