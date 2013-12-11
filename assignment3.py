a487df17543b66de25a970e3ed1bc688361b2f75
AngeloFajardo <acf11f@my.fsu.edu>
2013-12-10 20:14:53 -0500

    priority queue

--- assignment3.py
+++ assignment3.py
@@ -2,67 +2,121 @@
 # COP4531
 # Assignment 3
 
-from collections import namedtuple
+import itertools
+from heapq import heapify, heappush, heappop
 
 inf = float('inf')
-Edge = namedtuple('Edge', 'start, end, cost')
- 
-class Graph():
-    def __init__(self, edges):
-        self.edges = edges2 = [Edge(*edge) for edge in edges]
-        self.vertices = set(sum(([e.start, e.end] for e in edges2), []))
- 
-    def dijkstra(self, source):
-        dist = {vertex: inf for vertex in self.vertices}
-        previous = {vertex: None for vertex in self.vertices}
-        dist[source] = 0
-        q = self.vertices.copy()
-        neighbours = {vertex: set() for vertex in self.vertices}
-        for start, end, cost in self.edges:
-            neighbours[start].add((end, int(cost)))
+readFile = open("input.txt")
+
+class PriorityQueue():
+    def __init__(self):
+        self.pq = []
+        self.entry_finder = {}
+
+    def addNode(self, node, priority=0):
+        entry = [priority, node]
+        self.entry_finder[node] = entry
+        heappush(self.pq, entry)
+
+    def popNode(self):
+        priority, node = heappop(self.pq)
+        return (node, priority)
             
-        #pp(neighbours)
- 
-        while q:
-            u = min(q, key=lambda vertex: dist[vertex])
-            print(dist)
-            q.remove(u)
-            if dist[u] == inf:
-                break
-            for v, cost in neighbours[u]:
-                alt = dist[u] + cost
-                if alt < dist[v]:                                  # Relax (u,v,a)
-                    dist[v] = alt
-                    previous[v] = u
+    def printQueue(self):
+        for i in self.pq:
+            print (i[1], ':', i[0], '')
 
-        for x in dist:
-            print(x, dist[x])                   
+    def getPriority(self, value):
+        if value in self.entry_finder:
+            return self.entry_finder[value][0]
 
-def main() :
-    
-    inputFile = open("input.txt")
+def main():
 
-    first_line = inputFile.readline()   
-    if(first_line[0] == 'D'):           
-        print ("Directed\n")            
-    if(first_line[0] == 'U'):           
-        print ("Undirected\n")          
+    setMode = True
 
-    graph = []
-    nodeList = {}
-    
-    for line in inputFile:
+    G = []
+        
+    for line in readFile:
         if line[0] != '#':
-            nodeList = tuple(line.split())
-            graph.append(nodeList)
+            if setMode:
+                if line[0] == "D":
+                    print ('Directed')
+                elif line[0] == "UD":
+                    print ('Undirected')
+                setMode = False
+            else:
+                temp = line.split()
+                G.append((temp[0], temp[1], int(temp[2])))
 
-    graphs = Graph(graph)
-##    print(graphs.edges)
-##    print(graphs.vertices)
+    vertices = set()
 
-    print(graphs.dijkstra('A'))
+    for i in G:
+        vertices.add(i[0])
+        vertices.add(i[1])
 
+    
+    ##loop = True
+    ##source = ''
+    ##
+    ##while loop:
+    ##    if source not in vertices:
+    ##        source = raw_input('Enter Start Node: ')[0]
+    ##    else:
+    ##        loop = False
 
+    source = 'A'
+    edges = {vertex: set() for vertex in vertices} 
+
+    for start, end, cost in G:
+        edges[start].add((end, cost))
+
+    Q = PriorityQueue()
+
+    for i in vertices:
+        if i == source:
+            Q.addNode(i, 0)
+        else:
+            Q.addNode(i, inf)
+
+    uq = Q.popNode() # Pop min Starting node
+        
+    while(True):
+        if uq[1] == inf: break
+        uVal, uPrio = uq # uVal => Value    uPrio => Priority
+
+        for adjV in edges[uVal]: # for each adj vertex in edges in set index uVal
+            if uPrio + adjV[1] < Q.getPriority(adjV[0]):
+                newPrio = uPrio + adjV[1]
+                Q.addNode(adjV[0], newPrio)
+
+        
+        uq = Q.popNode() 
+
+    writer = open('out.txt', 'w')
+    writer.write('Dijkstra\n')
+    for i in Q.entry_finder:
+        writer.write('NODE ')
+        writer.write(i)
+        writer.write(' : ')
+        writer.write(str(Q.entry_finder[i][0]))
+        writer.write('\n')
+    writer.write('End Dijkstra')
+
+    
 if __name__ == '__main__':
-
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
